b38addd7d630182d3c79c931b68fac9af7bb915a
TautvisP <59112653+TautvisP@users.noreply.github.com>
2024-10-27 17:03:05 +0200

    Fixed bug with bullets not showing up for opponents

--- Tankistas/GameClient/src/clientSide/Commands/ShootCommand.java
+++ Tankistas/GameClient/src/clientSide/Commands/ShootCommand.java
@@ -1,17 +1,22 @@
 package clientSide.Commands;
 
+import clientSide.Client;
+import clientSide.Protocol;
 import clientSide.Tank;
 
 public class ShootCommand implements Command {
     private Tank tank;
+    private Client client;
 
     public ShootCommand(Tank tank) {
         this.tank = tank;
+        this.client = Client.getGameClient();
     }
 
     @Override
     public void execute() {
         System.out.println("ShootCommand");
+        client.sendToServer(new Protocol().ShotPacket(tank.getTankID()));
         tank.shot();
     }
 }
